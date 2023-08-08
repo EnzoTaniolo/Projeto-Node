0255aaea3be88a789d500f6dbde4f324161d58c0
EnzoTaniolo <enzotaniolofort@gmail.com>
2023-08-08 15:53:53 -0300

    Request com Post para criar um user e com get para sabermos as informações dos users

---
+++ Index.js
@@ -0,0 +1,52 @@
+const express = require("express")
+const uuid = require("uuid")
+
+const port = 3000
+const app = express()
+app.use(express.json())
+
+/*
+
+ - Query Params => meusite.com/users?nome=enzo&age=18 //FILTROS
+ - Rout Params => /users/2       // BUSCAR,DELETAR OU ATUALIZAR ALGO ESPECIFICO 
+ - Request Body => { "name": "Enzo, "Age": 18 }
+
+ GET => buscar informação no back-end
+ POST => criar informação no back-end
+ PUT/PATCH => Alterar/Atualizar informação no back-end
+ DELETE => deletar informação no back-end
+
+*/
+
+// Neste projeto guardaremos as informações dos users aqui para fins de simplificação, o correto seria em um Banco de dados
+const users = []
+
+app.get("/users", (request, response)=>{
+    return response.json(users)
+    // rota do tipo get que usaremos para ver todos os usuarios
+
+})
+
+app.post("/users", (request, response)=>{
+    const {name, age} = request.body
+
+    const user = { id:uuid.v4(), name, age }
+
+    users.push(user)
+
+    return response.status(201).json(user)
+
+})
+
+
+
+
+
+
+
+
+
+
+app.listen( port, ()=>{
+    console.log(`Server Started on port ${port}👍`)
+})
